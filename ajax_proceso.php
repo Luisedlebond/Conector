f0096d74719612459d9c70f2cc92238976289d19
luisedlebond <luisedlebond@gmail.com>
2015-11-29 00:23:06 +0000

    Procesos ajax y php

---
+++ ajax_proceso.php
@@ -0,0 +1,4 @@
+<?php 
+$resultado = $_POST['x1'] + $_POST['y1'] + $_POST['z1'] + $_POST['a1']+$_POST['x2'] + $_POST['y2'] + $_POST['z2'] + $_POST['a2']+$_POST['x3'] + $_POST['y3'] + $_POST['z3'] + $_POST['a3']; 
+echo $resultado;
+?>
